0a461cbbc4866a56a4823acbb2815d8a8fa8ca16
edioc <143817291+edioc@users.noreply.github.com>
2023-09-03 08:29:36 +0700

    Update universal_esp.lua

--- universal_esp.lua
+++ universal_esp.lua
@@ -1,1 +1,3 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/edioc/universal-esp/main/playernameesp.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/edioc/universal-esp/main/highlight.lua"))()
